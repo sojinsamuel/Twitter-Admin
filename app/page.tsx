60917e690abf081dcfb443afbb90d0214348093a
sojinsamuel <sojinsamuel2001@gmail.com>
2024-04-06 21:17:39 +0530

    Fix label

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,9 @@ export default function Home() {
 
   return (
     <div className="bg-gradient-to-br from-[#0f0f0f] to-[#222222] min-h-screen flex flex-col items-center justify-center text-white p-4 ">
-      <h1 className="text-6xl font-bold text-center">Twitter Bot Automation</h1>
+      <h1 className="text-6xl font-bold text-center">
+        Twitter Bot Automation!
+      </h1>
       <p className="mt-4 text-xl text-center">
         Powered by <span className="font-bold">AWS</span>
       </p>
